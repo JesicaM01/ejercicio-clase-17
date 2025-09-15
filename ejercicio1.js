9a64cf926f42290a0e916d3eb93b0ae4d42f6f44
¨JesicaM01¨ <jesymerep@gmail.com>
2025-09-15 11:05:00 -0300

    ejercicio3

--- ejercicio1.js
+++ ejercicio1.js
@@ -9,22 +9,39 @@
 
 
 
-const calcularDescuento = (edad) => {if (edad >= 65) {
- return 15;
-    } else  {
-return 0;
-}
+//const calcularDescuento = (edad) => {if (edad >= 65) {
+ //return 15;
+  //  } else  {
+//return 0;
+//}
+//};
+//const edadUsuario = parseInt(prompt("Por favor ingrese su edad:"));
+//if (isNaN(edadUsuario) || edadUsuario < 0) {
+// console.log("Por favor ingrese una edad válida.");
+//} else 
+  //  {
+// const descuento = calcularDescuento(edadUsuario);
+//if (descuento > 0) {
+ //console.log(`Felicidades! Tienes un Descuento del ${descuento} %`);
+ //} else
+   //  {
+ //console.log("Lo siento no aplica ningun descuento");
+  //  }
+//}
+
+
+
+const convertidorDolaresAPesos = (dolares,tasacambio) => {
+ return dolares * tasacambio;
 };
-const edadUsuario = parseInt(prompt("Por favor ingrese su edad:"));
-if (isNaN(edadUsuario) || edadUsuario < 0) {
- console.log("Por favor ingrese una edad válida.");
-} else 
-    {
- const descuento = calcularDescuento(edadUsuario);
-if (descuento > 0) {
- console.log(`Felicidades! Tienes un Descuento del ${descuento} %`);
- } else
-     {
- console.log("Lo siento no aplica ningun descuento");
-    }
+const dolaresIngresados = parseFloat(prompt("Por favor ingrese la cantidad en dólares que desea convertir a pesos:"));
+const tasaDeCambio = 1400; // Ejemplo de tasa de cambio
+const pesosConvertidos = convertidorDolaresAPesos(dolaresIngresados, tasaDeCambio);
+if (isNaN(dolaresIngresados) || isNaN(tasaDeCambio)) {
+console.log("Por favor ingrese valores numéricos válidos.");
+} else {
+console.log(`La cantidad en pesos es: ${pesosConvertidos}`);
+}
+if (isNaN(dolaresIngresados) || dolaresIngresados < 0) {
+console.log("Por favor ingrese una cantidad válida en dólares.");
 }
